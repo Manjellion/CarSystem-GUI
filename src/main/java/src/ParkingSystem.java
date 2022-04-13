9713de76f235e7c53731fc35a0f22b5b4959082e
Manjil Ghale <rilleymanjil@hotmail.com>
2022-04-13 22:37:49 +0100

    Update ParkingSystem.java

--- src/main/java/src/ParkingSystem.java
+++ src/main/java/src/ParkingSystem.java
@@ -6,33 +6,30 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 
 public class ParkingSystem extends Application{
 
+    //attributes
+    private int  noOfSpaces;
+    private parkedCarList list;
+
     private Label headingLabel = new Label("Parking Application");
+    private Label nameLabel = new Label("Name: ");
+    private TextField nameField = new TextField();
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    //methods
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Parking System");
-        Button priBtn = new Button();
-        priBtn.setText("Click here");
-        priBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello World");
-            }
-        });
-
-
-
+        primaryStage.setTitle("Parking Application"); // Title of the GUI App
+        
         StackPane layout = new StackPane();
-        layout.getChildren().add(priBtn);
-
+        layout.getChildren().addAll(headingLabel, nameLabel, nameField);
         primaryStage.setScene(new Scene(layout, 400, 400));
         primaryStage.show();
     }
