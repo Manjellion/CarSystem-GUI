2e50d4056aa304f975020aa08be31f141724def5
Manjil Ghale <rilleymanjil@hotmail.com>
2022-04-29 14:25:50 +0100

    Update ParkingSystem.java

--- src/main/java/src/ParkingSystem.java
+++ src/main/java/src/ParkingSystem.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -18,8 +19,8 @@ public class ParkingSystem extends Application{
     private parkedCarList list;
 
     //Width and Height
-    private final int WIDTH = 1080;
-    private final int HEIGHT = 640;
+    private final int WIDTH = 800;
+    private final int HEIGHT = 500;
 
     //javaFX components as visual components declared as attributes for the class
     //To Add, remove, display and save & quit event handling
@@ -29,8 +30,12 @@ public class ParkingSystem extends Application{
     private TextField nameAddField = new TextField();
     private Label idAddLabel = new Label("ID:");
     private TextField idField = new TextField();
-    private Label regisAddLabel = new Label("Registered, enter Yes or No:");
-    private TextField regisAddField = new TextField();
+<<<<<<< HEAD
+    private Label regisAddLabel = new Label("Is your car Registered: ");
+    private Button regisYes = new Button("Yes");
+    private Button regisNo = new Button("No");
+=======
+>>>>>>> 3619d146c71986af035bb376f07488cbf550c1b8
     private Button addBtn = new Button("Add to car park");
     private Button removeBtn = new Button("Remove from car park");
     private Button displayBtn = new Button("Display Parked Cars");
@@ -41,7 +46,7 @@ public class ParkingSystem extends Application{
     private Label idCheckerLabel = new Label("ID");
     private TextField idField2 = new TextField();
     private Button checkRegisBtn = new Button("Check if Registered");
-    private Button displayRegisBtn = new Button("Display Registered Cars");
+
     private TextArea displayRegisCar = new TextArea();
 
     //To register a car using their unique car plate number
@@ -68,33 +73,37 @@ public class ParkingSystem extends Application{
 
         // create HBox
         // To add registered cars
-        HBox carDetails = new HBox(20);
-        HBox parkedCarBtn = new HBox(20);
+        HBox carDetails = new HBox(10);
+        HBox parkedCarBtn = new HBox(10);
 
         // To display registered cars
-        HBox displayRegisDetails = new HBox(20);
-        HBox displayRegisteredBtn = new HBox(20);
+        HBox displayRegisDetails = new HBox(10);
+        HBox displayRegisteredBtn = new HBox(10);
 
         // To register a car
-        HBox registerDetails = new HBox(20);
-        HBox registerBtn = new HBox(20);
+        HBox registerDetails = new HBox(10);
+        HBox registerBtn = new HBox(10);
 
         // add components to HBox
-        carDetails.getChildren().addAll(nameAddLabel, nameAddField, idAddLabel, idField, regisAddLabel, regisAddField);
+<<<<<<< HEAD
+        carDetails.getChildren().addAll(nameAddLabel, nameAddField, idAddLabel, idField, regisAddLabel, regisYes, regisNo);
+=======
+        carDetails.getChildren().addAll(nameAddLabel, nameAddField, idAddLabel, idField);
+>>>>>>> 3619d146c71986af035bb376f07488cbf550c1b8
         parkedCarBtn.getChildren().addAll(addBtn, removeBtn, displayBtn, saveAndQuitBtn);
 
         displayRegisDetails.getChildren().addAll(idCheckerLabel, idField2);
-        displayRegisteredBtn.getChildren().addAll(checkRegisBtn, displayRegisBtn);
+        displayRegisteredBtn.getChildren().addAll(checkRegisBtn);
 
         registerDetails.getChildren().addAll(regisNameLabel, regisNameField, regisIdLabel, regisIdField);
         registerBtn.getChildren().addAll(regisBtn);
 
         //create VBox
-        VBox root = new VBox(15);
+        VBox root = new VBox(10);
         // add components to VBox
         root.getChildren().addAll(headingLabel, carDetails, parkedCarBtn, displayParkedCar, displayRegisDetails, displayRegisteredBtn, displayRegisCar, registerDetails, registerBtn, regisTextArea);
         // add VBox to the scene
-        Scene scene = new Scene(root, Color.AZURE);
+        Scene scene = new Scene(root);
 
         // set font for heading
         Font font = new Font("Montserrat", 40);
@@ -133,12 +142,9 @@ public class ParkingSystem extends Application{
         root.setMinSize(WIDTH, HEIGHT);
         root.setMaxSize(WIDTH, HEIGHT);
 
-        displayParkedCar.setMaxSize(WIDTH, HEIGHT / 5);
-        displayParkedCar.setPadding(new Insets(20));
-        displayRegisCar.setMaxSize(WIDTH, HEIGHT / 5);
-        displayRegisCar.setPadding(new Insets(20));
-        regisTextArea.setMaxSize(WIDTH, HEIGHT/ 5);
-        regisTextArea.setPadding(new Insets(20));
+        displayParkedCar.setMaxSize(WIDTH - 100, HEIGHT / 5);
+        displayRegisCar.setMaxSize(WIDTH - 100, HEIGHT / 5);
+        regisTextArea.setMaxSize(WIDTH - 100, HEIGHT/ 5);
         displayParkedCar.setEditable(false);
         displayRegisCar.setEditable(false);
         regisTextArea.setEditable(false);
@@ -147,20 +153,18 @@ public class ParkingSystem extends Application{
         primaryStage.setHeight(HEIGHT);
 
         // customise the visual components
-
         // customise the VBox border and background
         BorderStroke style = new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(0), new BorderWidths(2));
         root.setBorder(new Border(style));
         root.setBackground(Background.EMPTY);
 
         // customise buttons
-        addBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        displayBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        removeBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        saveAndQuitBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        checkRegisBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        displayRegisBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
-        regisBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(10), Insets.EMPTY)));
+        addBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
+        displayBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
+        removeBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
+        saveAndQuitBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
+        checkRegisBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
+        regisBtn.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, new CornerRadii(10), Insets.EMPTY)));
 
         // call private methods for button event handlers using lambda expression
         addBtn.setOnAction(e -> addHandler());
@@ -168,7 +172,6 @@ public class ParkingSystem extends Application{
         displayBtn.setOnAction(e -> displayHandler());
         saveAndQuitBtn.setOnAction(e -> saveAndQuitHandler());
         checkRegisBtn.setOnAction(e -> checkRegisteredHandler());
-        displayRegisBtn.setOnAction(e -> displayRegisHandler());
         regisBtn.setOnAction(e -> registerHandler());
 
         // configure the stage and make the stage visible
@@ -187,33 +190,35 @@ public class ParkingSystem extends Application{
         String response = dialog.showAndWait().get();
         return Integer.parseInt(response);
     }
-    // change regis field text to a button for yes and no to check if object is registered
+
     private void addHandler() {
         String nameEntered = nameAddField.getText();
         String idEntered = idField.getText();
-        String regisEntered = regisAddField.getText();
+<<<<<<< HEAD
+=======
+
+        parkedCar carObj = list.search(Integer.parseInt(idEntered));
+>>>>>>> 3619d146c71986af035bb376f07488cbf550c1b8
 
         // check for errors
         if(nameEntered.length() == 0 || idEntered.length() == 0) {
-            displayParkedCar.setText("Name must be entered");
-        } else if(Integer.parseInt(idEntered) == 0) {
-            displayParkedCar.setText("Please enter a valid ID");
+            displayParkedCar.setText("Name and ID must be entered must be entered");
         } else if(list.getTotal() == noOfSpaces){
             displayParkedCar.setText("The car park is currently full, please come back again later when there is space");
         } else { // okay to add the object
-            int id = Integer.parseInt(idEntered);
-            if(regisEntered == "Yes") {
-                parkedCar p = new parkedCar(nameEntered, id, true);
+<<<<<<< HEAD
+            if()
+=======
+            if(carObj == null) {
+                displayParkedCar.setText("The car you are  adding isn't registered, please register below");
+            } else {
+                parkedCar p = new parkedCar(nameEntered, Integer.parseInt(idEntered), true);
                 list.addParkedCar(p);
                 nameAddField.setText("");
                 idField.setText("");
-                regisAddField.setText("");
                 displayParkedCar.setText("Car for " + nameEntered + " has been successfully parked to the parking system");
-            } else if (regisEntered == "No") {
-                displayParkedCar.setText("Please register your car ID below");
-            } else {
-                displayParkedCar.setText("Please fill out the fields correctly");
             }
+>>>>>>> 3619d146c71986af035bb376f07488cbf550c1b8
         }
     }
     // Button will allow user to enter ID to remove the object holding the variable for ID value
@@ -221,7 +226,9 @@ public class ParkingSystem extends Application{
         String idEntered = idField.getText();
         int idParsed = Integer.parseInt(idEntered);
         // check for errors
-        if(list.getTotal() == noOfSpaces) {
+        if(idEntered.length() == 0) {
+            displayParkedCar.setText("ID must be entered");
+        } else if(list.getTotal() == noOfSpaces) {
             displayParkedCar.setText("The Parking space is full");
         } else if (list.search(idParsed) == null) {
             displayParkedCar.setText("The car doesn't exist in the car park");
@@ -232,13 +239,12 @@ public class ParkingSystem extends Application{
     }
     // Button will display all cars parked in the car park
     private void displayHandler(){
-        int i;
         if(list.isEmpty()) { // no objects to display as class collection is empty
             displayParkedCar.setText("The car park is empty");
         } else { // display the objects stored in the parkedCarList class collection
-            displayParkedCar.setText("NAME: " + "\t" + "ID: " + "\n");
-            for(i = 1; i <= list.getTotal(); i++) {
-                displayParkedCar.setText(list.getParkedCar(i).getName() + "\t\t" + list.getParkedCar(i).getID());
+            displayParkedCar.setText("NAME: " + "\t\t\t" + "ID: " + "\n");
+            for(int i = 1; i <= list.getTotal(); i++) {
+                displayParkedCar.appendText(list.getParkedCar(i).getName() + "\t\t\t" + list.getParkedCar(i).getID() + "\n");
             }
         }
     }
@@ -247,26 +253,19 @@ public class ParkingSystem extends Application{
         Platform.exit();
     }
     private void checkRegisteredHandler(){
-        int ID = Integer.parseInt(idField2.getText());
+        String idEntered = idField2.getText();
+        int ID = Integer.parseInt(idEntered);
         parkedCar carObj = list.search(ID);
 
-        if(carObj.checkCarRegistered() == true) {
-            displayRegisCar.setText("Car is Registered");
-        } else {
+        if(idEntered.length() == 0) {
+            displayRegisCar.setText("ID must be entered must be entered");
+        } else if(carObj == null) {
             displayRegisCar.setText("Car is not registered, please register below");
+        } else {
+            displayRegisCar.setText("Car is Registered, thank you for checking " + carObj.getName());
         }
     }
-    private void displayRegisHandler(){
-        int i;
-        if(list.isEmpty()) { // no objects to display as class collection is empty
-            displayRegisCar.setText("There are no registered data saved in the system");
-        } else { // display the objects stored in the class collection
-            displayRegisCar.setText("NAME: " + "\t" + "ID: " + "\n");
-            for(i = 1; i <= list.getTotal(); i++) {
-                displayRegisCar.setText(list.getParkedCar(i).getName() + "\t\t" + list.getParkedCar(i).getID());
-            }
-        }
-    }
+
     private void registerHandler(){
         String name = regisNameField.getText();
         int id = Integer.parseInt(regisIdField.getText());
@@ -276,7 +275,7 @@ public class ParkingSystem extends Application{
         } else {
             parkedCar carObj = new parkedCar(name, id, true);
             list.addParkedCar(carObj);
-            regisTextArea.setText("Car has been successfully registered to the system and has been added to the parking system automatically");
+            regisTextArea.setText("Car has been successfully registered to the system");
         }
     }
 
